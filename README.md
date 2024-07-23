fa2b07588db5acf6f9f0f3e8401e61f77069913d
HarinduChira <basnaya-ps19152@stu.kln.ac.lk>
2024-07-24 03:51:07 +0530

    Added Basic Components with libraries

--- README.md
+++
@@ -1,1 +0,0 @@
-# Productivity-and-Finance-Management-System
---
+++ Self_Up/Client/src/pages/Trash.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Trash = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Trash
+  
